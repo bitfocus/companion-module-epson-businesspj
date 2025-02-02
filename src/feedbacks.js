a7051ce5a38745c1126a531d3c9ac7389bc5edc7
h07d0q <46907193+h07d0q@users.noreply.github.com>
2025-02-02 23:28:34 +0100

    added feedbacks and preset with longpress
    added feedback and polling option in config. Also added preset for On/Off with longpress

--- src/feedbacks.js
+++ src/feedbacks.js
@@ -3,7 +3,33 @@ const { combineRgb } = require('@companion-module/base');
 module.exports = {
 	initFeedbacks: function () {
 		let self = this;
-		let feedbacks = {};
+		let feedbacks = {
+			'system_status': {
+				type: 'boolean',
+				name: 'System Status',
+				description: 'Returns Standby or Lamp On',
+				defaultStyle: {
+					bgcolor: combineRgb(255,0,0),
+					color: combineRgb(0,0,0),
+				},
+				options: [
+					{
+						type: 'dropdown',
+						label: 'Status',
+						id: 'status',
+						choices: [
+							{ id: 'Standby', label: 'Standby' },
+							{ id: 'Warm-up', label: 'Warm-up' },
+							{ id: 'OK', label: 'OK' }
+						],
+						default: 'Standby'
+					}
+				],
+				callback: (feedback) => {
+					return feedback.options.status === self.getVariableValue('system_status');
+				}
+			}
+		};
 
 		const foregroundColor = combineRgb(255, 255, 255) // White
 		const backgroundColorRed = combineRgb(255, 0, 0) // Red
